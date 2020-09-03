f3b5986c65f71252e265f027dc04f7c7784e2563
Michael Pfohl <rmpfohl@gmail.com>
2020-09-02 20:54:33 -0400

    build all quadrants

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,9 +1,12 @@
 import build from './components/buildQuadrant';
+import quadrants from './data/quadrantInfo';
 
 import '../styles/main.scss';
 
 const init = () => {
-  build.buildQuadrant('eat', 'healthy', 'unhealthy', '100');
+  quadrants.getQuadrants().forEach((quadrant) => {
+    build.buildQuadrant(quadrant.name, quadrant.button1, quadrant.button2, quadrant.defaultScore);
+  });
 };
 
 init();
